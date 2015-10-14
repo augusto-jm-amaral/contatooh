9cea7ce6d3ed188b130aabdc50c14d1c8ea4d6fd
Augusto José <augusto.jm.amaral1@gmail.com>
2015-10-14 14:54:55 -0300

    Corrigindo bug test.js

--- config/env/test.js
+++ config/env/test.js
@@ -7,7 +7,7 @@ module.exports = {
   travisJobNumber: process.env.TRAVIS_JOB_NUMBER,
   travisBuild: process.env.TRAVIS_BUILD,
   clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SERCRET,
+  clientSecret: process.env.CLIENT_SECRET,
   seleniumUser: process.env.SELENIUM_USER,
   seleniumUserPassword: process.env.SELENIUM_USER_PASSWORD
 };
